ab137c19e0094a1bec3245fc6e194cf8ef928882
jason chang <jasonchang6435@gmail.com>
2023-03-22 19:03:21 +0800

    chore: update edit md path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.png",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://docs.taskon.xyz",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -41,15 +41,14 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/Taskon-xyz/taskon-doc/edit/main/",
         },
         // blog: {
         //   showReadingTime: true,
         //   // Please change this to your repo.
         //   // Remove this to remove the "edit this page" links.
         //   editUrl:
-        //     "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        //     "https://github.com/Taskon-xyz/taskon-doc/edit/main/",
         // },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -79,7 +78,7 @@ const config = {
           // },
           // { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/Taskon-xyz/taskon-doc/tree/main/docs",
+            href: "https://github.com/Taskon-xyz/taskon-doc",
             label: "GitHub",
             position: "right",
           },
@@ -124,7 +123,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} TaskOn, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} TaskOn, Inc.`,
       },
       prism: {
         theme: lightCodeTheme,
